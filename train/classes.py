64dcc0df7c79be777454cd1479625bc00c1b016e
Tobias.Ludwig02 <tobias.ludwig@freudenberg.com>
2024-07-04 12:22:21 +0200

    Added Logger and deleted not used files

--- train/classes.py
+++ train/classes.py
@@ -1,6 +1,6 @@
 from stable_baselines3.common.callbacks import BaseCallback
 from flappy_bird_gymnasium.envs.flappy_bird_env import FlappyBirdEnv
-import matplotlib.pyplot as plt
+# from stable_baselines3.common.logger import configure
 import os
 import numpy as np
 import pandas as pd
@@ -20,31 +20,3 @@ class CustomFlappyBirdEnv(FlappyBirdEnv):
         elif self.pipe_passed:
             reward = 1.0  # +1.0 - successfully passing a pipe
         return reward
-
-class SaveEpisodeRewardCallback(BaseCallback):
-    def __init__(self, log_dir: str, verbose=1):
-        super(SaveEpisodeRewardCallback, self).__init__(verbose)
-        self.log_dir = log_dir
-        self.episode_rewards = []
-        self.current_rewards = []
-
-    def _on_step(self) -> bool:
-        rewards = self.locals['rewards']
-        self.current_rewards.append(rewards)
-
-        # Check if any environment is done
-        dones = self.locals['dones']
-        if any(dones):
-            # Calculate the total reward for this episode
-            episode_reward = sum(np.sum(reward) for reward in self.current_rewards)
-            self.episode_rewards.append({
-                'timesteps': self.num_timesteps,
-                'reward': episode_reward
-            })
-            self.current_rewards = []  # Reset for the next episode
-
-        return True
-
-    def on_training_end(self) -> None:
-        df = pd.DataFrame(self.episode_rewards)
-        df.to_csv(os.path.join(self.log_dir, 'episode_rewards.csv'), index=False)
--- train/train_custom_env.py
+++ train/train_custom_env.py
@@ -1,20 +1,26 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.logger import configure
 import os
 import numpy as np
 import pandas as pd
-from classes import CustomFlappyBirdEnv, SaveEpisodeRewardCallback
+from classes import CustomFlappyBirdEnv
 
-# Log-Directory für den Callback festlegen
+# Log-Directory und Dateiname für den Callback festlegen
 log_dir = "./logs/"
 os.makedirs(log_dir, exist_ok=True)
+custom_log_file = os.path.join(log_dir, "std_2Mio.csv")
+
+# Configure the logger to save data to a specific folder
+new_logger = configure(log_dir, ["stdout", "csv"])
 
 gym.envs.registration.register(
     id='CustomFlappyBird-v0',
     entry_point='__main__:CustomFlappyBirdEnv',
     max_episode_steps=10000000,
 )
+
 # Environment erstellen und mit VecMonitor wrappen
 env = make_vec_env("CustomFlappyBird-v0", n_envs=4, env_kwargs={'render_mode': 'rgb_array', 'use_lidar': False})
 
@@ -32,12 +38,12 @@ model = PPO(
     device='cuda'
 )
 
-# Callback erstellen
-callback = SaveEpisodeRewardCallback(log_dir=log_dir)
+# Attach the new logger to the model
+model.set_logger(new_logger)
 
 # Modell trainieren mit Callback
-model.learn(total_timesteps=300000, callback=callback)
+model.learn(total_timesteps=2000000)
 
 # Modell speichern
-# model.save("models/model_10Mio")
+model.save("models/std_2Mio")
 
